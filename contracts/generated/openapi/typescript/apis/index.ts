209475adfa990bc7a951a0af35cce5ce33f6f723
vicdenz <david.daniliuc24@gmail.com>
2026-08-05 03:15:10 -0400

    Define versioned application contracts

---
+++ contracts/generated/openapi/typescript/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './FavoritesApi';
+export * from './FundamentalsApi';
+export * from './IdentityApi';
+export * from './ModelsApi';
+export * from './OperationsApi';
+export * from './PortfoliosApi';
+export * from './ReportsApi';
